292d9f87e457171e85592e74981248b8bfcede67
Matthew <matt.askett+github@gmail.com>
2022-09-01 19:41:48 +0900

    Removed console.log()

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import data from "./data"
 const locations = data.sort(function(a,b){
   return new Date(b.startDate) - new Date(a.startDate)
 }).map(location => <Location key={location.id} {...location} />)
-console.log(locations)
 function App() {
   return (
     <>
